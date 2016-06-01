0ac250c952f83d3216e6865d3aaa72de2b0ba23d
agracia95 <alvaro.gracia.delarica@alumnos.upm.es>
2016-06-02 00:13:02 +0200

    Tema 15: Editar preguntas

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var session = require('express-session');
 var partials = require('express-partials');
 var flash = require('express-flash');
+var methodOverride = require('method-override');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -23,6 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(session({ secret: "Quiz", resave: false, saveUninitialized: true}));
+app.use(methodOverride('_method', {methods: ["POST", "GET"]}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(partials());
--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -17,7 +17,7 @@ exports.load = function(req, res, next, quizId) {
 exports.index = function(req, res, next) {
 	models.Quiz.findAll().then(function(quizzes) {
 		res.render('quizzes/index.ejs', { quizzes: quizzes });
-	})
+	}).catch(function(error) { next(error) });
 };
 
 // GET /quizzes/:id
@@ -31,6 +31,12 @@ exports.show = function(req, res, next){
 			throw new Error('No existe ese quiz en la BBDD.');
 		}
 	}).catch(function(error) { next(error); });
+};
+
+// GET /quizzes/:id/edit
+exports.edit = function(req, res, next){
+	var quiz = req.quiz;
+	res.render('quizzes/edit', { quiz: quiz });
 };
 
 // GET /quizzes/:id/check
@@ -72,6 +78,29 @@ exports.create = function(req, res, next) {
 		res.render('quizzes/new', { quiz: quiz });
 	}).catch(function(error) {
 		req.flash('error', 'Error al crear el Quiz: ' + error.message);
+		next(error);
+	});
+};
+
+// PUT /quizzes/:id
+exports.update = function(req, res, next) {
+
+	req.quiz.question = req.body.quiz.question;
+	req.quiz.answer = req.body.quiz.answer;
+
+	req.quiz.save({fields: ["question", "answer"]}).then(function(quiz) {
+		req.flash('success', 'Quiz editado con éxito.');
+		res.redirect('/quizzes');
+	}).catch(Sequelize.ValidationError, function(error) {
+
+		req.flash('error', 'Errores en el formulario:');
+		for (var i in error.errors) {
+			req.flash('error', error.errors[i].value);
+		};
+
+		res.render('/quizzes/edit', { quiz: req.quiz });
+	}).catch(function(error) {
+		req.flash('error', 'Error al editar el Quiz: ' + error.message);
 		next(error);
 	});
 };
